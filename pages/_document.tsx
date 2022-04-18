f262ac93626ae53ba28e92cab2dbcd93bec9fe04
yuta <eypc.ofun@gmail.com>
2022-04-18 19:37:49 +0900

    install MUI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,61 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import * as React from "react";
+import { RenderPageResult } from "next/dist/shared/lib/utils";
+import Document, {Html, Head, Main, NextScript, DocumentInitialProps} from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../createEmotionCache";
+import theme from "../theme";
 
-const MyDocument = () => {
-    return (
-        <Html lang="ja-JP">
-            <Head>
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-    );
+export default class MyDocument extends Document {
+    render(): JSX.Element {
+        return (
+            <Html lang="ja">
+                <Head>
+                    {/* PWA primary color */}
+                    <meta name="theme-color" content={theme.palette.primary.main} />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+MyDocument.getInitialProps = async (ctx): Promise<DocumentInitialProps> => {
+    const originalRenderPage = ctx.renderPage;
+
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
+        originalRenderPage({
+            enhanceApp:
+                (App: any) =>
+                    // eslint-disable-next-line react/display-name
+                    (props): JSX.Element =>
+                        <App emotionCache={cache} {...props} />,
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+            data-emotion={`${style.key} ${style.ids.join(" ")}`}
+            key={style.key}
+            dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+    ));
+
+    return {
+        ...initialProps,
+        styles: [
+            ...React.Children.toArray(initialProps.styles),
+            ...emotionStyleTags,
+        ],
+    };
 };
-
-export default MyDocument;
---
+++ theme.tsx
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+// set design here
+});
+
+export default theme;
