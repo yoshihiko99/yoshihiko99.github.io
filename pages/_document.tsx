adb36bbe7e586cf510d390afc92448c8a53343f6
yuta <eypc.ofun@gmail.com>
2022-04-18 19:11:01 +0900

    install tailwind

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const MyDocument = () => {
+    return (
+        <Html lang="ja-JP">
+            <Head>
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default MyDocument;
