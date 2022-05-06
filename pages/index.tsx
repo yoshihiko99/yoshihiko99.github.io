ef95e8d2e033aebb4382f50b9ce2c8971bb0578e
yuta <eypc.ofun@gmail.com>
2022-05-06 16:19:02 +0900

    add works

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,19 @@ import Terminal from '@/components/terminal-like'
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
 import 'animate.css'
+import Card from '@/components/Card'
 
 const Home: NextPage = () => {
   const [refAboutMe, inViewAboutMe] = useInView({
-    rootMargin: '-50px',
+    rootMargin: '-100px',
     triggerOnce: true
   })
-  const [refWorks, inViewWorks] = useInView({
-    rootMargin: '-50px',
+  const [refWorksTeleLabo, inViewWorksTeleLabo] = useInView({
+    rootMargin: '-100px',
+    triggerOnce: true
+  })
+  const [refWorksMyNote, inViewWorksMyNote] = useInView({
+    rootMargin: '-100px',
     triggerOnce: true
   })
 
@@ -44,41 +49,48 @@ const Home: NextPage = () => {
           />
         </div>
 
-        <div className="h-screen flex items-center justify-center">
-          <div ref={refAboutMe} className="">
-            {inViewAboutMe && (
-              <div className="animate__animated animate__fadeInUp w-full grid sm:grid-cols-2">
-                <div>
-                  <h2 className="text-5xl mb-3">About Me</h2>
-                  <p>
-                    東京工業大学大学院情報理工学院、現在修士一年。神奈川県出身のエンジニア。
-                    大学3年の夏、スキルアップをするため東大発ベンチャー「株式会社Drafty」に所属する。
-                    テックリードとしてエンジニアチームのリーダーを就任、メンバー7人を統率する。
-                    2年間のインターン業務を経て、AWSクラウドサーバー構築、Webアプリケーション（フロントエンド・バックエンド）作成、社内用Pythonツールなど、幅広く開発業務を行う。
-                  </p>
-                </div>
+        <div ref={refAboutMe} className="py-28">
+          <h2 className="text-5xl mb-10 w-full">About Me</h2>
+          {inViewAboutMe && (
+            <div className="animate__animated animate__fadeInUp w-full grid sm:grid-cols-2">
+              <div>
+                <p>
+                  東京工業大学大学院情報理工学院、現在修士一年。神奈川県出身のエンジニア。
+                  大学3年の夏、スキルアップをするため東大発ベンチャー「株式会社Drafty」に所属する。
+                  テックリードとしてエンジニアチームのリーダーを就任、メンバー7人を統率する。
+                  2年間のインターン業務を経て、AWSクラウドサーバー構築、Webアプリケーション（フロントエンド・バックエンド）作成、社内用Pythonツールなど、幅広く開発業務を行う。
+                </p>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
 
-        <div className="h-screen flex items-center justify-center">
-          <div ref={refWorks} className="my-auto">
-            {inViewWorks && (
-              <div className="animate__animated animate__fadeInUp w-full grid sm:grid-cols-2">
-                <div>
-                  <h2 className="text-5xl mb-3">Works</h2>
-                  <div className="mb-10">
-                    <h3 className="text-2xl mb-3 font-bold">テレラボ</h3>
-                    <p>
-                      テストです。テストです。テストです。テストです。テストです。テストです。
-                    </p>
-                  </div>
-                  <div className="mb-10">
-                    <h3 className="text-2xl mb-3 font-bold">MyNote</h3>
-                    <p>テストです</p>
-                  </div>
-                </div>
+        <div ref={refWorksTeleLabo} className="py-10">
+          <h2 className="text-5xl mb-3 w-full">Works</h2>
+          {inViewWorksTeleLabo && (
+            <div className="animate__animated animate__fadeInUp mb-10">
+              <Card
+                ImageSrc="/tele_labo_image.jpeg"
+                ImageAlt="テレラボのイメージ"
+                leading="テレラボ"
+                content="test"
+                tags={['Laravel', 'PHP', 'JavaScript', 'AWS', 'MySQL']}
+                link="https://tele-labo.jp/"
+              />
+            </div>
+          )}
+
+          <div ref={refWorksMyNote} className="mb-10">
+            {inViewWorksMyNote && (
+              <div className="animate__animated animate__fadeInUp mb-10">
+                <Card
+                  ImageSrc="/no_image.png"
+                  ImageAlt="MyNoteのイメージ"
+                  leading="MyNote"
+                  content="test"
+                  tags={['Next.js', 'React', 'Laravel', 'PHP', 'AWS', 'MySQL']}
+                  link="https://tele-labo.jp/"
+                />
               </div>
             )}
           </div>
